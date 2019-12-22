aa82179370a4f52de803ebaa8996e4fb6d24f624
ypablo <yfedora@gmail.com>
2019-12-22 19:49:37 +0000

    Map

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ class App extends Component {
  constructor() {
    super()
    this.state ={
-      username: "Tola"
+      username: "Tola",
+      showPerson: false,
+      person: [
+        {name:"Zen", age: 25},
+        {name: "Yen", age: 38},
+        {name: "Lee", age: 33}
+      ]
    }
  }
 
 switchNameHandler = (newname) => {
   this.setState({
-     username: newname
-     
+     username: newname,   
   })
 }
 
@@ -25,13 +30,33 @@ nameChangeHandler = (event) => {
   })
 }
 
+toggleShowHandler =() => {
+ const doesShow = this.state.showPerson
+ const defaultName = "Tola"
+ this.setState({showPerson: !doesShow, username: defaultName})
+}
+
+deletePersonHandler =(personIndex) => {
+  const persons = this.state.person
+  persons.splice(personIndex, 1)
+  this.setState({person: persons})
+}
+
 render() {  
   return (
     <div className="App">
+      <button onClick={this.toggleShowHandler}>Toggle text</button>
       <UserInput changed={this.nameChangeHandler} value={this.state.username}/>
       <button onClick={() => this.switchNameHandler("Lolek")}>Change name</button>
-      <UserOutput username={this.state.username}>That's it</UserOutput>
-      <UserOutput username="Max">Whatever and ever</UserOutput>
+      {this.state.showPerson ?
+        <div>
+          <UserOutput username={this.state.username}>That's it</UserOutput>
+          <UserOutput username="Max">Whatever and ever</UserOutput>
+        </div> : null
+      }
+      {this.state.person.map((person, index) => {
+        return <div click={()=>this.deletePersonHandler(index)}>{`${person.name} ${person.age}`}</div>
+      })}
     </div>
   );
   }
